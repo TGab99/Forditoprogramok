9889b0a4de25f0c974af819dd03703edb4035b9f
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-01-01 18:13:08 +0100

    Recautomata

---
+++ Recautomata/Recautomata/Recautomata/Automata.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Recautomata
+{
+    public class Automata
+    {
+        string input;
+        int i = 0;
+
+        public string simple(string st)
+        {
+            return Regex.Replace(st, "([0-9]+)", "i");
+        }
+
+        public Automata(string input)
+        {
+            Console.WriteLine("Az eredeti input: {0}", input);
+            this.input = $"{simple(input)}#";
+            Console.WriteLine("Az egyszerűsített input: {0}", this.input);
+        }
+
+        public void elfogad(char ch)
+        {
+            if (input[i] != ch)
+            {
+                Console.WriteLine("Hibás input : {0}. Helytelen karakter: {1}", input, input[i]);
+            }
+            i++;
+        }
+
+        public void S()
+        {
+            E();
+            elfogad('#');
+            Console.WriteLine("Az elemzés lefutott!");
+        }
+
+        void E()
+        {
+            T();
+            Ev();
+        }
+
+        void T()
+        {
+            F();
+            Tv();
+        }
+
+        void Ev()
+        {
+            if(input[i] == '+')
+            {
+                elfogad('+');
+                T();
+                Ev();
+            }
+        }
+
+        void Tv()
+        {
+            if(input[i] == '*')
+            {
+                elfogad('*');
+                F();
+                Tv();
+            }
+        }
+
+        void F()
+        {
+            if(input[i] == '(')
+            {
+                elfogad('(');
+                E();
+                elfogad(')');
+            }
+            else
+            {
+                elfogad('i');
+            }
+        }
+    }
+}
---
+++ Recautomata/Recautomata/Recautomata/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recautomata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automata automata = new Automata("(31+22)*55");
+            automata.S();
+        }
+    }
+}
