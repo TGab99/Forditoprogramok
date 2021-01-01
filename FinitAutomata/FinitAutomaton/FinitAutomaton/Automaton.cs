714efb5eaa9e911ec3a8ec89112ba70eacf17d21
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-01-01 18:11:59 +0100

    FinitAutomata

---
+++ FinitAutomata/FinitAutomaton/FinitAutomaton/Automaton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinitAutomaton
+{
+    public class Automaton
+    {
+        string state = "A";
+        Dictionary<String, String> D = new Dictionary<string, string>();
+
+        public void prepare()
+        {
+            D.Add("A+", "B");
+            D.Add("A-", "B");
+            D.Add("Ad", "C");
+            D.Add("Bd", "C");
+            D.Add("Cd", "C");
+        }
+
+        public char convert(char c)
+        {
+            if (Char.IsDigit(c))
+            {
+                return 'd';
+            }
+
+            return c;
+        }
+
+        public string delta(string st, char act)
+        {
+            if (D.ContainsKey(st + convert(act)))
+            {
+                return D[st + convert(act)];
+            }
+
+            return "Error";
+        }
+
+        public void main(string input)
+        {
+            int i = 0;
+            while(i < input.Length && state != "Error")
+            {
+                state = delta(state, input[i]);
+                i++;
+            }
+
+            if(state != "Error")
+            {
+                Console.WriteLine("{0} helyes bemenő adat", input);
+            }
+            else
+            {
+                Console.WriteLine(
+                    "{0} nem helyes bemenő adat. Hibás karakter található {1}. helyen!", input, i);
+            }
+        }
+    }
+}
---
+++ FinitAutomata/FinitAutomaton/FinitAutomaton/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FinitAutomaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automaton A = new Automaton();
+
+            string content;
+            List<string> list = new List<string>();
+
+            try
+            {
+                StreamReader SR = new StreamReader(
+                    File.OpenRead(@"/Users/torokgabriella/Projects/FinitAutomaton/FinitAutomaton/text/input.txt"));
+                while (!SR.EndOfStream)
+                {
+                    content = SR.ReadLine();
+                    string[] spieces = content.Split(' ');
+                    for(int i = 0; i < spieces.Length; i++)
+                    {
+                        list.Add(spieces[i]);
+                    }
+
+                }
+                SR.Close();
+
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            A.prepare();
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                A.main(list[i]);
+            }
+        }
+    }
+}
